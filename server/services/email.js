69580a6f63f9afa204eceda913fcf82da8a0e7bc
SHUMAIL AHMAD SAMAR <shumailahmad8938@gmail.com>
2026-05-24 14:25:21 +0530

    testing email

--- server/services/email.js
+++ server/services/email.js
@@ -2,11 +2,16 @@ import nodemailer from "nodemailer";
 
 const createTransporter = () => {
   return nodemailer.createTransport({
-    service: "gmail",
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.GMAIL_USER,
       pass: process.env.GMAIL_PASS,
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 };
 
